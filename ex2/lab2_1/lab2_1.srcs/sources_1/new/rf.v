e18ca189801b748b8927f98c06f988841d164906
jx_2 <jiaxin_desu@163.com>
2021-07-01 17:51:49 +0800

    新增了实验二的工程文件夹

---
+++ ex2/lab2_1/lab2_1.srcs/sources_1/new/rf.v
@@ -0,0 +1,66 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2021/07/01 16:24:59
+// Design Name: 
+// Module Name: rf
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module rf(
+    input               clk_i,
+    input               rst_n_i,
+    input       [4:0]   rr1_i,
+    input       [4:0]   rr2_i,
+    input       [4:0]   wr_i,
+    input       [31:0]  wd_i,
+    input               we,
+    output  reg [31:0]  rd1_o,
+    output  reg [31:0]  rd2_o
+    );
+    reg [31:0]  x0;
+    reg [31:0]  x1;
+    reg [31:0]  x2;
+    reg [31:0]  x3;
+    reg [31:0]  x4;
+    reg [31:0]  x5;
+    reg [31:0]  x6;
+    reg [31:0]  x7;
+    reg [31:0]  x8;
+    reg [31:0]  x9;
+    reg [31:0]  x10;
+    reg [31:0]  x11;
+    reg [31:0]  x12;
+    reg [31:0]  x13;
+    reg [31:0]  x14;
+    reg [31:0]  x15;
+    reg [31:0]  x16;
+    reg [31:0]  x17;
+    reg [31:0]  x18;
+    reg [31:0]  x19;
+    reg [31:0]  x20;
+    reg [31:0]  x21;
+    reg [31:0]  x22;
+    reg [31:0]  x23;
+    reg [31:0]  x24;
+    reg [31:0]  x25;
+    reg [31:0]  x26;
+    reg [31:0]  x27;
+    reg [31:0]  x28;
+    reg [31:0]  x29;
+    reg [31:0]  x30;
+    reg [31:0]  x31;
+endmodule
---
+++ ex2/lab2_1/lab2_1.srcs/sources_1/new/sext.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2021/07/01 16:24:59
+// Design Name: 
+// Module Name: sext
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module sext(
+    
+    );
+endmodule
