59d4e037403059f9e12757742ce9c3f81efe70cb
jx_2 <jiaxin_desu@163.com>
2021-07-10 22:59:50 +0800

    已完成上板所需内容，只剩下上板测试

---
+++ ex2/lab2/lab2.srcs/constrs_1/new/constraints_top.xdc
@@ -0,0 +1,43 @@
+set_property PACKAGE_PIN Y18 [get_ports clk]
+set_property PACKAGE_PIN Y9 [get_ports rst_n]
+
+set_property PACKAGE_PIN C19 [get_ports led0_en]
+set_property PACKAGE_PIN E19 [get_ports led1_en]
+set_property PACKAGE_PIN D19 [get_ports led2_en]
+set_property PACKAGE_PIN F18 [get_ports led3_en]
+set_property PACKAGE_PIN E18 [get_ports led4_en]
+set_property PACKAGE_PIN B20 [get_ports led5_en]
+set_property PACKAGE_PIN A20 [get_ports led6_en]
+set_property PACKAGE_PIN A18 [get_ports led7_en]
+
+set_property PACKAGE_PIN F15 [get_ports led_ca]
+set_property PACKAGE_PIN F13 [get_ports led_cb]
+set_property PACKAGE_PIN F14 [get_ports led_cc]
+set_property PACKAGE_PIN F16 [get_ports led_cd]
+set_property PACKAGE_PIN E17 [get_ports led_ce]
+set_property PACKAGE_PIN C14 [get_ports led_cf]
+set_property PACKAGE_PIN C15 [get_ports led_cg]
+set_property PACKAGE_PIN E13 [get_ports led_dp]
+
+
+set_property IOSTANDARD LVCMOS33 [get_ports clk]
+set_property IOSTANDARD LVCMOS33 [get_ports rst_n]
+
+set_property IOSTANDARD LVCMOS33 [get_ports led0_en]
+set_property IOSTANDARD LVCMOS33 [get_ports led1_en]
+set_property IOSTANDARD LVCMOS33 [get_ports led2_en]
+set_property IOSTANDARD LVCMOS33 [get_ports led3_en]
+set_property IOSTANDARD LVCMOS33 [get_ports led4_en]
+set_property IOSTANDARD LVCMOS33 [get_ports led5_en]
+set_property IOSTANDARD LVCMOS33 [get_ports led6_en]
+set_property IOSTANDARD LVCMOS33 [get_ports led7_en]
+
+set_property IOSTANDARD LVCMOS33 [get_ports led_ca]
+set_property IOSTANDARD LVCMOS33 [get_ports led_cb]
+set_property IOSTANDARD LVCMOS33 [get_ports led_cc]
+set_property IOSTANDARD LVCMOS33 [get_ports led_cd]
+set_property IOSTANDARD LVCMOS33 [get_ports led_ce]
+set_property IOSTANDARD LVCMOS33 [get_ports led_cf]
+set_property IOSTANDARD LVCMOS33 [get_ports led_cg]
+set_property IOSTANDARD LVCMOS33 [get_ports led_dp]
+
--- ex2/lab2/lab2.srcs/sources_1/new/top.v
+++ ex2/lab2/lab2.srcs/sources_1/new/top.v
@@ -22,17 +22,31 @@
 
 
 module top(
-    input   clk,
-    input   rst_n,
-    output                  debug_wb_have_inst,
-    output   [31:0]       debug_wb_pc,
-    output                  debug_wb_ena,
-    output   [4:0]         debug_wb_reg,
-    output    [31:0]      debug_wb_value
+    input               clk,
+    input               rst_n,
+    output  reg         led0_en,
+    output  reg         led1_en,
+    output  reg         led2_en,
+    output  reg         led3_en,
+    output  reg         led4_en,
+    output  reg         led5_en,
+    output  reg         led6_en,
+    output  reg         led7_en,
+    output  reg         led_ca ,
+    output  reg         led_cb ,
+    output  reg         led_cc ,
+    output  reg         led_cd ,
+    output  reg         led_ce ,
+    output  reg         led_cf ,
+    output  reg         led_cg ,
+    output  reg         led_dp
     );
     // 连接信号
     // 时钟
     wire clk_cpu;
+    wire clk_lock;
+    wire pll_clk;
+    wire clk_disp;
     // CPU
     // 输入
     wire [31:0] cpu_irom_inst;
@@ -60,12 +74,44 @@ module top(
     wire dram_memwr;
     // 输出
     wire [31:0] dram_rd_data;
+    // DISPLAY
+    // 输入
+    wire display_busy;
+    wire [7:0] display_z1;
+    wire [7:0] display_r1;
+    wire [7:0] display_z2;
+    wire [7:0] display_r2;
+    // 输出
+    wire display_led0_en;
+    wire display_led1_en;
+    wire display_led2_en;
+    wire display_led3_en;
+    wire display_led4_en;
+    wire display_led5_en;
+    wire display_led6_en;
+    wire display_led7_en;
+    wire display_led_ca;
+    wire display_led_cb;
+    wire display_led_cc;
+    wire display_led_cd;
+    wire display_led_ce;
+    wire display_led_cf;
+    wire display_led_cg;
+    wire display_led_dp;
     
     // 时钟部件
     cpuclk U_cpuclk_0(
         .clk_in1    (clk),
-        .clk_out1   (clk_cpu)
+        .locked     (clk_lock),
+        .clk_out1   (pll_clk)
     );
+    assign clk_cpu = pll_clk & clk_lock;
+    divider U_divider_0(
+        .clk_i      (clk),
+        .rst_n_i    (rst_n),
+        .clk_disp   (clk_disp)
+    );
+    
     // cpu部件
     cpu U_cpu_0(
         .clk_cpu            (clk_cpu),
@@ -76,6 +122,7 @@ module top(
         .dram_addr          (cpu_dram_addr),
         .dram_wr_data       (cpu_dram_wr_data),
         .dram_memwr         (cpu_dram_memwr),
+        // 输出测试用的debug信号
         .debug_wb_have_inst (cpu_debug_wb_have_inst),
         .debug_wb_pc        (cpu_debug_wb_pc),
         .debug_wb_ena       (cpu_debug_wb_ena),
@@ -95,6 +142,32 @@ module top(
         .wr_data_i  (dram_wr_data),
         .rd_data_o  (dram_rd_data)
     );
+    // display 部件
+    display U_display_0(
+        .clk            (clk_disp),
+        .rst_n          (rst_n),
+        .busy           (display_busy),
+        .z1             (display_z1),
+        .r1             (display_r1),
+        .z2             (display_z2),
+        .r2             (display_r2),
+        .led0_en        (display_led0_en),
+        .led1_en        (display_led1_en),
+        .led2_en        (display_led2_en),
+        .led3_en        (display_led3_en),
+        .led4_en        (display_led4_en),
+        .led5_en        (display_led5_en),
+        .led6_en        (display_led6_en),
+        .led7_en        (display_led7_en),
+        .led_ca         (display_led_ca),
+        .led_cb         (display_led_cb),
+        .led_cc         (display_led_cc),
+        .led_cd         (display_led_cd),
+        .led_ce         (display_led_ce),
+        .led_cf         (display_led_cf),
+        .led_cg         (display_led_cg),
+        .led_dp         (display_led_dp)
+    );
     
     // 开始连线
     // CPU
@@ -106,11 +179,60 @@ module top(
     assign dram_addr    =   cpu_dram_addr;
     assign dram_wr_data =   cpu_dram_wr_data;
     assign dram_memwr   =   cpu_dram_memwr;
-
-    // 输出测试用的信号
-    assign debug_wb_have_inst = cpu_debug_wb_have_inst;
-    assign debug_wb_pc = cpu_debug_wb_pc;
-    assign debug_wb_ena = cpu_debug_wb_ena;
-    assign debug_wb_reg = cpu_debug_wb_reg;
-    assign debug_wb_value = cpu_debug_wb_value;
+    // DISPLAY
+    assign display_busy = 1'b0;
+    assign display_z1   = cpu_debug_wb_pc[31:24];
+    assign display_r1   = cpu_debug_wb_pc[23:16];
+    assign display_z2   = cpu_debug_wb_pc[15:8];
+    assign display_r2   = cpu_debug_wb_pc[7:0];
+    
+    // 输出信号
+    always @ (*) begin
+        led0_en = display_led0_en;
+    end
+    always @ (*) begin
+        led1_en = display_led1_en;
+    end
+    always @ (*) begin
+        led2_en = display_led2_en;
+    end
+    always @ (*) begin
+        led3_en = display_led3_en;
+    end
+    always @ (*) begin
+        led4_en = display_led4_en;
+    end
+    always @ (*) begin
+        led5_en = display_led5_en;
+    end
+    always @ (*) begin
+        led6_en = display_led6_en;
+    end
+    always @ (*) begin
+        led7_en = display_led7_en;
+    end
+    always @ (*) begin
+        led_ca = display_led_ca;
+    end
+    always @ (*) begin
+        led_cb = display_led_cb;
+    end
+    always @ (*) begin
+        led_cc = display_led_cc;
+    end
+    always @ (*) begin
+        led_cd = display_led_cd;
+    end
+    always @ (*) begin
+        led_ce = display_led_ce;
+    end
+    always @ (*) begin
+        led_cf = display_led_cf;
+    end
+    always @ (*) begin
+        led_cg = display_led_cg;
+    end
+    always @ (*) begin
+        led_dp = display_led_dp;
+    end
 endmodule
